1a170bcc44b764c9f20ec6e9fde46c3ab4fa365d
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-05-28 18:28:29 +0200

    add align_yaxis() method

--- utils/other.py
+++ utils/other.py
@@ -10,3 +10,18 @@ def correct_bars(xs, ys, patches, bwidth):
 
     for clip_box,bar in zip(clip_boxes,patches):
         bar.set_clip_path(clip_box.get_path(), bar.get_transform())
+
+
+def align_yaxis(ax1, v1, ax2, v2):
+    '''
+    adjust ax2 ylimit so that v2 in ax2 
+    is aligned to v1 in ax1
+
+    taken from https://stackoverflow.com/a/10482477/692634
+    '''
+    _, y1 = ax1.transData.transform((0, v1))
+    _, y2 = ax2.transData.transform((0, v2))
+    inv = ax2.transData.inverted()
+    _, dy = inv.transform((0, 0)) - inv.transform((0, y1-y2))
+    miny, maxy = ax2.get_ylim()
+    ax2.set_ylim(miny+dy, maxy+dy)
