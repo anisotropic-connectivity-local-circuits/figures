3ecc66a4257e60a20808b719f0cf418d6350e4d0
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-05-29 00:33:27 +0200

    final clean-up of fig4, closes #4

--- main/fig4_three_motifs.py
+++ main/fig4_three_motifs.py
@@ -86,10 +86,10 @@ pl.rcParams['text.latex.preamble'] = [
     r'\sisetup{detect-all}',    # force siunitx to use the fonts
 ]  
 
-pl.rcParams['xtick.major.pad']=+45
+pl.rcParams['xtick.major.pad']=+45.5
 
 fig = pl.figure(facecolor="white")
-fig.set_size_inches(7.2,2.15)
+fig.set_size_inches(7.4,2.25)
 
 # motifs 1-14 drawn on yscale of ax1
 # motifs 15-16 drawn on yscale of ax2
@@ -171,10 +171,9 @@ plot_data(rlc_dist, errs_dist, color['dist'],
 
 
 
-
+# legend - not currently independent of axis vals
 lbl_fntsz = 10
 tick_fntsz = 9
-
 
 xrect=0.9225
 ystart=4.685
@@ -206,50 +205,48 @@ fig.text(0.225,0.65, r'distance-dependent', color = 'black',
 
 
 
-pl.xticks(range(1,18), range(1,15)+['',15,16])
-#ax.xaxis.set_ticks(range(1,14),[str(i) for i in range(4,17)])
-
-
+# axes setup
 ax1.spines['bottom'].set_visible(False)
-ax2.spines['bottom'].set_visible(False)
-
-ax1.axhline(1,0,0.8025, zorder=39, color='k')
-ax1.axhline(1,0.855,1., zorder=39, color='k')
-
-ax1.axvline(15.18, 0.025,0.92, color='k',
-            linestyle='dashed', linewidth=1.5, dashes=(5, 2.5))
-
-ax1.tick_params(axis='both', which='major', labelsize=tick_fntsz)
-
-ax1.set_xlim(0.,17+1.25)
-
 ax1.spines['bottom'].set_position(('data',1))
 ax1.spines['right'].set_color('none')
 ax1.spines['top'].set_color('none')
 ax1.xaxis.set_ticks_position('none')
 #ax.yaxis.set_ticks_position('left')
 
-ax1.set_ylabel('relative counts', size=lbl_fntsz)
-
-
-for tick_label in ax2.get_yticklabels():
-    tick_label.set_fontsize(tick_fntsz)
-
-ax2.set_ylim(top=10) #!!
-
-
-
-ymin = 0
-ymax = 5
-ax1.set_ylim(ymin, ymax)
-
-ax2.yaxis.set_ticks(range(0,10,2))
-
+ax2.spines['bottom'].set_visible(False)
 ax2.spines['bottom'].set_position(('data',1))
 ax2.spines['left'].set_color('none')
 ax2.spines['top'].set_color('none')
 ax2.xaxis.set_ticks_position('none')
 
+# substitute broken up x-axis
+ax1.axhline(1,0,0.8025, zorder=39, color='k')
+ax1.axhline(1,0.855,1., zorder=39, color='k')
+
+# draw dashed separation
+ax1.axvline(15.18, 0.025,0.92, color='k',
+            linestyle='dashed', linewidth=1.5, dashes=(5, 2.5))
+
+
+ax1.set_xticks(range(1,18))
+ax1.set_xticklabels(range(1,15)+['',15,16])
+
+ax1.tick_params(axis='both', which='major', labelsize=tick_fntsz)
+ax1.set_ylabel('relative counts', size=lbl_fntsz)
+
+for tick_label in ax2.get_yticklabels():
+    tick_label.set_fontsize(tick_fntsz)
+
+
+ax1.set_xlim(0.,17+1.25)
+ymin, ymax = 0, 5 
+ax1.set_ylim(ymin, ymax)
+
+ax2.set_ylim(top=10) 
+ax2.yaxis.set_ticks(range(0,10,2))
+
+# make sure that y=1 is aligned
+#between ax1 and ax2
 align_yaxis(ax1, 1, ax2, 1)
 
 for i in range(1,18):
@@ -259,6 +256,3 @@ for i in range(1,18):
 path='fig4_three_motifs.png'
 fig.savefig(path, dpi=300, bbox_inches='tight')
 
-#fig.savefig("/users/hoffmann/Downloads/"+label+params.extension, dpi=params.dpi, bbox_inches='tight')
-
-pl.close('all')
